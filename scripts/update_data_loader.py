e2bcf6ecdfa3d27ad86ed0d42ff5e893d3a158cc
Tim Hogarty <thogarty@equinix.com>
2024-11-06 17:19:06 -0800

    Update data schemas to align with design and include data loader update script

---
+++ scripts/update_data_loader.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import json
+
+def find_json_files(directory):
+    json_files = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.json') and file != "catalog.json":
+                json_files.append(os.path.join(root, file))
+    return json_files
+
+def retrieve_supported_events(files):
+    events = set()
+    for file in files:
+        with open(file, "r") as eventFile:
+            data = json.load(eventFile)
+            for supportedEvent in data["cloudeventTypes"]:
+                events.add(supportedEvent)
+
+    return events
+
+def writeSupportedEventsToDataLoaderFile(supportedEvents):
+    with open("./SupportedEventTypes_DataLoader.json", "w") as eventsFile:
+        eventsFile.write(json.dumps(sorted(supportedEvents), indent=4))
+        eventsFile.write("\n")
+
+
+if __name__ == "__main__":
+    # Example usage
+
+    directory_to_search = sys.argv[1]
+    json_files = find_json_files(directory_to_search)
+
+    for file in json_files:
+        print(file)
+
+    supportedEvents = retrieve_supported_events(json_files)
+    writeSupportedEventsToDataLoaderFile(supportedEvents)
