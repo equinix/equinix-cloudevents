4c022abbdd38ef97fc43b750ade3a7cb0afc43dc
Tim Hogarty <thogarty@equinix.com>
2024-11-08 16:34:30 -0800

    Update paths processing of jsonschema directory from data_loader script

--- scripts/update_data_loader.py
+++ scripts/update_data_loader.py
@@ -1,23 +1,16 @@
 import os
-import sys
 import json
 
-def find_json_files(directory):
-    json_files = []
+def retrieve_supported_events():
+    directory = os.path.dirname(os.path.abspath(__file__)) + '/../jsonschema'
+    events = set()
     for root, dirs, files in os.walk(directory):
+        print(root)
         for file in files:
             if file.endswith('.json') and file != "catalog.json":
-                json_files.append(os.path.join(root, file))
-    return json_files
-
-def retrieve_supported_events(files):
-    events = set()
-    for file in files:
-        with open(file, "r") as eventFile:
-            data = json.load(eventFile)
-            for supportedEvent in data["cloudeventTypes"]:
-                events.add(supportedEvent)
-
+                with open(root + "/" + file, "r") as eventFile:
+                    data = json.load(eventFile)
+                    events.update(data["cloudeventTypes"])
     return events
 
 def writeSupportedEventsToDataLoaderFile(supportedEvents):
@@ -27,8 +20,5 @@ def writeSupportedEventsToDataLoaderFile(supportedEvents):
 
 
 if __name__ == "__main__":
-    directory_to_search = sys.argv[1]
-    json_files = find_json_files(directory_to_search)
-
-    supportedEvents = retrieve_supported_events(json_files)
+    supportedEvents = retrieve_supported_events()
     writeSupportedEventsToDataLoaderFile(supportedEvents)
