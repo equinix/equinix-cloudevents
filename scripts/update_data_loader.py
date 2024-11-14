78f7d9da8020d4bd776e9888f04d1c2355108d8a
Tim Hogarty <thogarty@equinix.com>
2024-11-13 16:32:02 -0800

    Update the event names

--- scripts/update_data_loader.py
+++ scripts/update_data_loader.py
@@ -6,15 +6,16 @@ def retrieve_supported_events():
     events = set()
     for root, dirs, files in os.walk(directory):
         for file in files:
+
             if file.endswith('.json') and file != "catalog.json":
                 with open(root + "/" + file, "r") as eventFile:
                     data = json.load(eventFile)
                     events.update(data["cloudeventTypes"])
-    return events
+    return sorted(events)
 
 def writeSupportedEventsToDataLoaderFile(supportedEvents):
-    with open("./SupportedEventTypes_DataLoader.json", "w") as eventsFile:
-        eventsFile.write(json.dumps(sorted(supportedEvents), indent=4))
+    with open(os.path.dirname(os.path.abspath(__file__)) + "/../SupportedEventTypes_DataLoader.json", "w") as eventsFile:
+        eventsFile.write(json.dumps(supportedEvents, indent=4))
         eventsFile.write("\n")
 
 
