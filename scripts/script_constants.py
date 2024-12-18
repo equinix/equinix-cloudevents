9f4de7a446d70d191dee2aaf9537b8ec878c9d09
tili <tili@ap.equinix.com>
2024-12-19 01:07:02 +0800

    CRH Cloud event update

---
+++ scripts/script_constants.py
@@ -0,0 +1,10 @@
+RELEASED = "released"
+PREVIEW = "preview"
+
+EVENTS = "cloudeventTypes"
+METRICS = "metricNames"
+ALERTS = "alertNames"
+
+README_EVENTS = "Events"
+README_METRICS = "Metrics"
+README_ALERTS = "Alerts"
