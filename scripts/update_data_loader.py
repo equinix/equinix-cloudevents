23a9a563e3b095694a087ca7123cce317950281b
srushti-patl <srpatel@equinix.com>
2025-03-25 17:02:52 -0700

    feat: adding new SLA table and updating data schema

--- scripts/update_data_loader.py
+++ scripts/update_data_loader.py
@@ -6,20 +6,17 @@ def main():
     supportedEvents = retrieve_supported_events()
     writeSupportedEventsToDataLoaderFile(supportedEvents)
 
-
-def filterName(listOfDicts):
-    return [d["name"] for d in listOfDicts]
-
 def retrieve_supported_events():
     directory = os.path.dirname(os.path.abspath(__file__)) + '/../jsonschema'
     dataLoaderStructure = {}
     for root, dirs, files in os.walk(directory):
         for file in files:
-            if file.endswith('.json') and file != "catalog.json":
+            if file.endswith('.json') and file != "catalog.json" and file != "slaCategories.json":
                 with open(root + "/" + file, "r") as eventFile:
                     domain = root.split("/")[-2]
                     data = json.load(eventFile)
                     if domain not in dataLoaderStructure:
+                        print(f"filename: {file}")
                         dataLoaderStructure[domain] = {
                             sc.EVENTS: {
                                 sc.RELEASED: [],
@@ -34,13 +31,28 @@ def retrieve_supported_events():
                                 sc.PREVIEW: []
                             }
                         }
-                    dataLoaderStructure[domain][sc.EVENTS][sc.RELEASED].extend(filterName(data[sc.EVENTS][sc.RELEASED]))
-                    dataLoaderStructure[domain][sc.EVENTS][sc.PREVIEW].extend(filterName(data[sc.EVENTS][sc.PREVIEW]))
-                    dataLoaderStructure[domain][sc.METRICS][sc.RELEASED].extend(filterName(data[sc.METRICS][sc.RELEASED]))
-                    dataLoaderStructure[domain][sc.METRICS][sc.PREVIEW].extend(filterName(data[sc.METRICS][sc.PREVIEW]))
-                    dataLoaderStructure[domain][sc.ALERTS][sc.RELEASED].extend(filterName(data[sc.ALERTS][sc.RELEASED]))
-                    dataLoaderStructure[domain][sc.ALERTS][sc.PREVIEW].extend(filterName(data[sc.ALERTS][sc.PREVIEW]))
+                    
+                    for event in data.get(sc.EVENTS, []):
+                        if isinstance(event, dict):
+                            if event.get("isReleased", True):
+                                dataLoaderStructure[domain][sc.EVENTS][sc.RELEASED].append(event["name"])
+                            if event.get("inPreview", True):
+                                dataLoaderStructure[domain][sc.EVENTS][sc.PREVIEW].append(event["name"])
 
+                    for metric in data.get(sc.METRICS, []):
+                        if isinstance(metric, dict):
+                            if metric.get("isReleased", True):
+                                dataLoaderStructure[domain][sc.METRICS][sc.RELEASED].append(metric["name"])
+                            if metric.get("inPreview", True):
+                                dataLoaderStructure[domain][sc.METRICS][sc.PREVIEW].append(metric["name"])
+
+                    for alert in data.get(sc.ALERTS, []):
+                        if isinstance(alert, dict):
+                            if alert.get("isReleased", True):
+                                dataLoaderStructure[domain][sc.ALERTS][sc.RELEASED].append(alert["name"])
+                            if alert.get("inPreview", True):
+                                dataLoaderStructure[domain][sc.ALERTS][sc.PREVIEW].append(alert["name"])
+                    
                     dataLoaderStructure[domain][sc.EVENTS][sc.RELEASED] = sorted(set(dataLoaderStructure[domain][sc.EVENTS][sc.RELEASED]))
                     dataLoaderStructure[domain][sc.EVENTS][sc.PREVIEW] = sorted(set(dataLoaderStructure[domain][sc.EVENTS][sc.PREVIEW]))
                     dataLoaderStructure[domain][sc.METRICS][sc.RELEASED] = sorted(set(dataLoaderStructure[domain][sc.METRICS][sc.RELEASED]))
