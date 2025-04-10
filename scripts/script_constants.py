c56dd4c180a0b331011b6a3b2415f5a9cdf22c6c
srushti-patl <srpatel@equinix.com>
2025-04-09 21:38:21 -0700

    fix: Updating SLO table with defintions

--- scripts/script_constants.py
+++ scripts/script_constants.py
@@ -5,6 +5,16 @@ EVENTS = "cloudeventTypes"
 METRICS = "metricNames"
 ALERTS = "alertNames"
 
-README_EVENTS = "Events"
-README_METRICS = "Metrics"
-README_ALERTS = "Alerts"
+README = {
+    EVENTS: "Events",
+    METRICS: "Metrics",
+    ALERTS: "Alerts"    
+}
+CATEGORY_COLORS = {
+    "GREEN_EVENT_SLO": "33cbff",
+    "RED_EVENT_SLO": "33ff6b",
+    "RED_METRIC_SLO":"ff8733",
+    "YELLOW_METRIC_SLO": "a87efd",
+    "stream_metric_slo": "fd7e89",
+    "GREEN_ALERT_SLO": "#FFC300"
+}
